49e5479bd579d15861fbf05d2270bf5eb65f96fa
Harry Carron <hcarron92@gmail.com>
2026-01-13 22:33:15 +0000

    started tools

--- src/components/editor/workspace/Workspace.tsx
+++ src/components/editor/workspace/Workspace.tsx
@@ -3,17 +3,25 @@ import { EditorTool } from "@/types/editor/EditorTools";
 import { World } from "../world/World";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Tools from "../tools/Tools";
+import { useState } from "react";
 
-interface WorkspaceProps {
-  editorTool: EditorTool;
-}
+export function Workspace() {
+  const [editorTool, setEditorTool] = useState<EditorTool | undefined>();
 
-export function Workspace({ editorTool }: WorkspaceProps) {
-  const classes = "";
+  let classes = "";
+
+  if (editorTool === EditorTool.pan) {
+    classes = "cursor-grab";
+  }
+
+  if (editorTool === EditorTool.mag) {
+    classes += "cursor-crosshair";
+  }
 
   return (
-    <div className={"w-full h-full relative" + classes}>
+    <div className={"w-full h-full relative " + classes}>
       <TransformWrapper
+        panning={{ disabled: editorTool !== EditorTool.pan }}
         minScale={0.3}
         maxScale={1}
         wheel={{ smoothStep: 0.001, step: 0.2 }}
@@ -22,6 +30,10 @@ export function Workspace({ editorTool }: WorkspaceProps) {
           <World />
         </TransformComponent>
       </TransformWrapper>
+
+      <div className="absolute tools flex justify-items-center align-items-center m-2">
+        <Tools editorTool={editorTool!} setEditorTool={setEditorTool} />
+      </div>
     </div>
   );
 }
