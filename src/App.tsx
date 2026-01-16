fe1ef2c2996f50c0950d7d8a420fdea787d99db4
Harry Carron <hcarron92@gmail.com>
2026-01-16 19:57:43 +0000

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,15 @@ import "./App.css";
 import Menu from "./components/editor/menu/Menu";
 import { menuItems } from "./components/editor/menu/menu-items";
 import { Workspace } from "./components/editor/workspace/Workspace";
-import { useState } from "react";
-import type { EditorTool } from "./types/editor/EditorTools";
 
 function App() {
-  const [editorTool, setEditorTool] = useState<EditorTool>();
-
   return (
     <div className="flex h-full flex">
-      <div className="w-100">
+      <div className="w-80">
         <Menu navItems={menuItems} />
       </div>
       <div className="flex-1 min-w-0">
-        <Workspace editorTool={editorTool!} />
+        <Workspace />
       </div>
     </div>
   );
